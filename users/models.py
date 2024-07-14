b8d9115795fb1691d99c0a96233248d7fecda39c
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-07-14 23:03:37 +0300

    Подключен Django + два приложения users and habits

---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class User(AbstractUser):
+    """Модель пользователя."""
+
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35, verbose_name="Телефон", help_text="Укажите телефон", **NULLABLE
+    )
+    city = models.CharField(max_length=150, verbose_name="Город", **NULLABLE)
+    last_login = models.DateTimeField(
+        auto_now=True, verbose_name="Время последнего посещения", **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
