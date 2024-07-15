5afa76a65ab43a20b3aaad68cbb6a67bb24da2a5
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-07-15 22:56:48 +0300

    Пагинация

--- habits/views.py
+++ habits/views.py
@@ -1,32 +1,39 @@
 from rest_framework.viewsets import generics
 
 from habits.models import Habit
+from habits.paginations import HabitsPagination
 from habits.serializers import HabitSerializer
 
 
 class HabitsCreateAPIView(generics.CreateAPIView):
+    """ Создание привычки. """
     serializer_class = HabitSerializer
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer):  # функция автора с его привычкой
         habit = serializer.save()
         habit.owner = self.request.user
         habit.save()
 
 
 class HabitsListAPIView(generics.ListAPIView):
+    """ Список привычек. """
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    pagination_class = HabitsPagination
 
 
 class HabitsRetrieveAPIView(generics.RetrieveAPIView):
+    """ Просмотр одной привычки. """
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
 
 class HabitsUpdateAPIView(generics.UpdateAPIView):
+    """ Редактирование привычки. """
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
 
 class HabitsDestroyAPIView(generics.DestroyAPIView):
+    """ Удаление привычки. """
     queryset = Habit.objects.all()
