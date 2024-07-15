309f79effe8aa0f83e1f673bbe0d385854e360e2
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-07-15 19:29:28 +0300

    Валидация на добавление в связанные привычки только приятных

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
 from habits.models import Habit
+from habits.validators import validate_duration, SimultaneousSelection, ConnectionHabitCheck
 
 
 class HabitSerializer(ModelSerializer):
+    duration = serializers.IntegerField(validators=[validate_duration])
 
     class Meta:
         model = Habit
         fields = "__all__"
+
+        validators = [
+            SimultaneousSelection("connection_habit", "reward"),
+            ConnectionHabitCheck()
+        ]
---
+++ habits/validators.py
@@ -0,0 +1,29 @@
+from rest_framework.exceptions import ValidationError
+
+
+class SimultaneousSelection:
+    """Валидация на совместный выбор 'связанной привычки' и 'вознаграждения'."""
+
+    def __init__(self, connection_habit, reward):
+        self.field_1 = connection_habit
+        self.field_2 = reward
+
+    def __call__(self, habit):
+        if habit.get(self.field_1) and habit.get(self.field_2):
+            raise ValidationError(
+                "Нельзя одновременно выбирать связанную привычку и вознаграждение"
+            )
+
+
+def validate_duration(duration):
+    if duration and duration > 120:
+        raise ValidationError("время выполнения не должно превышать 120 секунд")
+
+
+class ConnectionHabitCheck:
+
+    def __call__(self, habit):
+        if not habit.get("connection_habit").nice_habit_bool:
+            raise ValidationError(
+                "В связанные привычки могут попасть только - приятные..."
+            )
