8b3a5bb2ceb2073e2e392cb924c61f0342360a0b
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-07-17 16:38:06 +0300

    доработки функции отправки

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,23 +1,17 @@
 import datetime
 from habits.services import send_telegram_message
-import pytz
 from celery import shared_task
-from django.core.mail import send_mail
 
 from habits.models import Habit
 from users.models import User
 
-zone = pytz.timezone("Europe/Moscow")
-
 
 @shared_task
 def tg_message():
-    #  print("1111111111111111111111111111111111111")
     users = User.objects.all()
-    habits = Habit.objects.all()
-    current_time = datetime.datetime.now().time()
 
     for user in users:
+        habits = Habit.objects.filter(owner=user)
         for habit in habits:
             if habit.owner == user:
                 chat_id = user.tg_chat_id
