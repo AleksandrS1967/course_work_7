40fef290245782b01e3316a424b768d9d1a7864d
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-07-26 16:20:44 +0300

    отключил предупреждение подключения брокера во время запуска в Celery 6.0 и выше

--- config/celery.py
+++ config/celery.py
@@ -2,12 +2,17 @@ import os
 
 from celery import Celery
 
+# Установка переменной окружения для настроек проекта
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# Создание экземпляра объекта Celery
 app = Celery("config")
 
+# Загрузка настроек из файла Django
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+# Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
 
-#  настройки для celery
+# отключает предупреждение подключения брокера во время запуска в Celery 6.0 и выше
+app.conf.broker_connection_retry_on_startup = True
